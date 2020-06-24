f5b8362f04be6e8f7faac6c4b238a4884146d739
unknown <jwade1327@gmail.com>
2020-06-24 12:02:12 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,7 +13,12 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+// function to generate a random numeric value
+var randomNumber = function() {
+    var value = Math.floor(Math.random() * 21) + 40;
 
+    return value;
+};
 
 // function to start a new game
 var fight = function(enemyName) {
@@ -30,17 +35,16 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
 
-         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
-        console.log(
-            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-        );
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
     
         // check enemy's health
         if (enemyHealth <= 0) {
@@ -55,11 +59,10 @@ var fight = function(enemyName) {
             window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
 
-        // remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
-        console.log(
-            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-        );
+        // generate random damage value based on enemy's attack power
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
 
         // check player's health
         if (playerHealth <= 0) {
@@ -77,6 +80,7 @@ var fight = function(enemyName) {
         playerHealth = 100;
         playerAttack = 10;
         playerMoney = 10;
+        
 
         for (var i =0; i < enemyNames.length; i++) {
             if (playerHealth > 0) {
@@ -87,7 +91,16 @@ var fight = function(enemyName) {
                 var pickedEnemyName = enemyNames[i];
 
                 // reset enemyHealth before starting new fight
-                enemyHealth = 50;
+                enemyHealth = Math.floor(Math.random() * 21) + 40;
+                
+                
+                var randomNumber = function(min, max) {
+                    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+                    return value;
+                };
+
+                enemyHealth= randomNumber(40, 60);
 
                 // use debugger to pause script from running and check what's going on at that moment in the code
                 // debugger;
@@ -190,6 +203,7 @@ var shop = function(){
 
 //start the game when the page loads
 startGame();
+
     
    
 
